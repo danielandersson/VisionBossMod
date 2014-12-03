a8aba0dced798637852326ffb05ca8315f17652e
kyau <kyau@kyau.org>
2014-12-02 18:00:19 -0800

    Version 3.051
    * Version bump for Highmaul
    * The Butcher & Trash before (Krush) has been added

--- InstancesWOD/Highmaul.lua
+++ InstancesWOD/Highmaul.lua
@@ -54,4 +54,68 @@ VBM_LoadInstance["Highmaul"] = function()
         emotes = {
         },
     };
+    --[[ ** The Butcher ** ]]--
+    VBM_BOSS_DATA["The Butcher"] = {
+		start = function()
+            VBM_BossTimer(6*60,"Berserk",VBM_ICONS.."spell_shadow_unholyfrenzy");
+        end,
+        debuffs = {
+            ["The Tenderizer"] = {VBM_WarnTextIcon("The Tenderizer","inv_mace_52"),function()
+                vbm_say("The Tenderizer - "..VBM_YOU);
+            end},
+        },
+        spells = {
+			--tank stacks
+            ["The Cleaver"] = {
+				event = "SPELL_AURA_APPLIED_DOSE",
+				spell = "The Cleaver",
+				amount = 3,
+				logic = ">",
+				dest = VBM_YOU,
+                simonsound = true,
+				func = function(s,d,a)
+					vbm_debuffwarn(vbm_c_r.."* * * The Cleaver - "..a.." Stacks * * *");
+                    vbm_say("The Cleaver - "..a.." Stacks - "..VBM_YOU);
+				end,
+			},
+			--warning dps if too many stacks
+			["Gushing Wounds"] = {
+				event = "SPELL_AURA_APPLIED_DOSE",
+				spell = "Gushing Wounds",
+				amount = 3,
+				logic = ">",
+				dest = VBM_YOU,
+                simonsound = true,
+				func = function(s,d,a)
+					vbm_debuffwarn(vbm_c_r.."* * * Gushing Wounds - "..a.." Stacks * * *");
+				end,
+			},
+        },
+        during = function()
+            --display The Butcher's power
+            local target = VBM_GetUnitReferens("The Butcher");
+            if(target) then
+                if(UnitPower(target) > 74) then
+                    vbm_infowarn("Energy ("..vbm_c_w..UnitPower(target)..vbm_c.."%)",0.1);
+                    VBM_PlayRepeatWarnSound(VBM_LOWER_DONG_SOUND,4);
+                end
+            end
+        end,
+    };
+    --[[ ** Trash ** ]]--
+    VBM_BOSS_DATA["Trash"] = {
+        spells = {
+            ["Boar's Rush"] = {
+                event = "SPELL_CAST_SUCCESS",
+                src = "Krush",
+                spell = "Boar's Rush",
+                func = function(s,d)
+                    vbm_bigwarn(vbm_c_t.."Boar's Rush on >>"..vbm_c_w..d..vbm_c_t.."<<");
+                    if(d==VBM_YOU) then
+                        vbm_say("Boar's Rush - "..VBM_YOU);
+                    end
+                end,
+            },
+        },
+    };
 end
